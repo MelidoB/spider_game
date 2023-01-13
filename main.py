0a90cabaf130a9d38d462be3f88d8c6354b83322
MelidoB <48568341+MelidoB@users.noreply.github.com>
2023-01-12 21:48:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from close_game import close_game_function
 
 
 def main():
+    #Pasare todo lo que se valla ah dibujar aqui
+    todo_para_dibujar = []
+    
     pygame.init()
     gameDisplay = pygame.display.set_mode((1400, 800))
     clock = pygame.time.Clock()
 
     while True:
         close_game_function()
-        gameDisplay.fill((255, 255, 255))
-
-#Hello
-
+        gameDisplay.fill((255, 255, 255)
+                         
+        #Dibujare todo lo que se valla ah dibujar aqui
+	 
+						
         clock.tick(60)
         pygame.display.update()
 
