81fbf4616937d044016cc59cf26a3461e27aac50
MelidoB <enrriquebello10@gmail.com>
2023-01-12 21:06:28 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- classes/personaje.py
+++ classes/personaje.py
@@ -1,7 +1,7 @@
 from classes.objeto import Modulo_De_Objeto
 
 
-class Modulo_De_Barra_De_Vida(Modulo_De_Objeto):
+class Modulo_De_Personaje(Modulo_De_Objeto):
     def __init__(self,nombre,posicion):
         Modulo_De_Objeto.__init__(nombre,posicion)
         self.habilidad = None
--- classes/projectiles.py
+++ classes/projectiles.py
@@ -1,6 +1,18 @@
 from classes.objeto import Modulo_De_Objeto
 
 
-class Modulo_De_Barra_De_Vida(Modulo_De_Objeto):
+class Modulo_De_Projectiles(Modulo_De_Objeto):
     def __init__(self,nombre,posicion):
         Modulo_De_Objeto.__init__(nombre,posicion)
+        self.animacion_de_movimiento = None
+        self.animacion_de_muerte = None
+        self.sonido_de_muerte = None
+
+    def cambiar_animacion_de_movimiento(self, nueva_animacion):
+        self.animacion_de_movimiento = nueva_animacion
+
+    def cambiar_animacion_de_muerte(self, nueva_animacion):
+        self.animacion_de_muerte = nueva_animacion
+
+    def cambiar_sonido_de_muerte(self, nuevo_sonido):
+        self.sonido_de_muerte = nuevo_sonido
